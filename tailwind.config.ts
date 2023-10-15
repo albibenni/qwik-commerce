b799ca4885521396692d83a1b5270c12a497f968
Alberto Benatti <albi.benni8@gmail.com>
2023-10-15 18:20:44 +0200

    fix configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./routes/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
